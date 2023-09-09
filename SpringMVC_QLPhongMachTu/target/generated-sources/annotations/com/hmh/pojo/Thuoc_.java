b21010ce054fe043dadca04786c4b97f461e1b61
huynhMinhHoang <2051052046hoang@ou.edu.vn>
2023-09-10 05:27:03 +0700

    new

--- SpringMVC_QLPhongMachTu/target/generated-sources/annotations/com/hmh/pojo/Thuoc_.java
+++ SpringMVC_QLPhongMachTu/target/generated-sources/annotations/com/hmh/pojo/Thuoc_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-09T23:12:39")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-10T05:05:29")
 @StaticMetamodel(Thuoc.class)
 public class Thuoc_ { 
 
